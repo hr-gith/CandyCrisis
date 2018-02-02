6a9cfa3ba76865b5694b2f1c9ddaab09659eaab6
Lakshay Verma <lakshayverma629@gmail.com>
2018-02-02 16:06:09 -0500

    Added Properties to Board

--- src/Models/Board.java
+++ src/Models/Board.java
@@ -4,4 +4,8 @@ package Models;
  * Created by hamideh on 02/02/2018.
  */
 public class Board {
+    private int length;
+    private int width;
+    private Token emptyTokenRef;
+    private Token[][] gridToken;
 }
