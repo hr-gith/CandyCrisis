e66f1ca0c878b9ff368df037e29dc4cd59ab41f1
hr-gith <hamideh.riazat@gmail.com>
2018-02-02 15:59:37 -0500

    initial commit

---
+++ src/Models/Board.java
@@ -0,0 +1,7 @@
+package Models;
+
+/**
+ * Created by hamideh on 02/02/2018.
+ */
+public class Board {
+}
---
+++ src/Models/Token.java
@@ -0,0 +1,48 @@
+package Models;
+
+/**
+ * Created by hamideh on 02/02/2018.
+ */
+public class Token {
+    private char sign;
+    private int xPos;
+    private int yPos;
+
+    public char getSign() {
+        return sign;
+    }
+
+    public void setSign(char sign) {
+        this.sign = sign;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public boolean moveUp (){
+
+    }
+
+    public boolean moveDown (){
+
+    }
+    public boolean moveRight (){
+
+    }
+    public boolean moveLeft (){
+
+    }
+}
